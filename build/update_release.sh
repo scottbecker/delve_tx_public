c5d4696dffeb350afbb5deb438880af868c0e0d6
Scott Becker <scottjbecker@gmail.com>
2016-11-16 16:24:34 -0500

    update public protocols

--- build/update_release.sh
+++ build/update_release.sh
@@ -3,5 +3,5 @@ set -e
 python increase_version.py
 python set_test_mode_false.py
 bash make_zip.sh
-transcriptic upload-release release.zip "DelveTX2"
+transcriptic upload-release release.zip "DelveTX2" || { echo 'my_command failed' ; exit 1; }
 bash publish_public_protocols.sh
